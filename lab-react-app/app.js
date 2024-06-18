bc91dd3b380c998ecda23bc3a4061b928a9fa3c7
niranjanaothayoth <160855491+niranjanaothayoth@users.noreply.github.com>
2024-06-18 21:02:36 +0530

    Add files via upload

---
+++ lab-react-app/app.js
@@ -0,0 +1,14 @@
+// app.js
+
+class ReactContainer extends React.Component {
+    render() {
+        return (
+            <div>Hello! Welcome to Kalvium
+                <div>This is babel</div>
+            </div>
+        );
+    }
+}
+
+const container = document.getElementById('react-container');
+ReactDOM.render(<ReactContainer />, container);
